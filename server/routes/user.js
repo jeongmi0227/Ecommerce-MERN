0816e939330ee29c0138e4ca3b06ba16d0aa5448
Jeong Mi Park <parkjeongmi0227@gmail.com>
2023-01-26 22:06:19 -0500

    Implemented JWT

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,66 @@
+const CryptoJs = require('crypto-js');
+const User = require('../models/User');
 const router = require('express').Router();
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verfityToken');
+
+router.put('/:id', verifyTokenAndAuthorization,async(req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJs.AES.encrypt(req.body.password,
+            process.env.PASS_SEC).toString();
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true });
+
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 
+// Delete user
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json('User has been deleted');
+    } catch (err) {
+        res.status(500).json(err);
+   }
+});
+
+// Get user
+router.get('/find/:id', verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+   }
+});
+
+// Get All users
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new;
+
+    try {
+        const users = query
+            ? await User.find().sort({ _id: -1 }).limit(5)
+            : await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+   }
+});
+
+
+// GET USER STATS
+
+router.get('/stats', verifyTokenAndAdmin, async (req, res) => {
+    
+});
 module.exports = router;
